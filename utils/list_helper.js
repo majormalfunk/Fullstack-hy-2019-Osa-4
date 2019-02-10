709c61432b2e3ec470762a392a0d55658c34c00b
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2019-02-10 20:02:19 +0200

    4.4: apufunktioita ja yksikkötestejä, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  var initialValue = 0
+  var sum = blogs.reduce(
+    (acc, blog) => acc + blog.likes
+    , initialValue)
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
