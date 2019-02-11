1bd265f186cebfb2c3278fd407d8d6a4650430c1
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2019-02-11 07:38:06 +0200

    4.5*: apufunktioita ja yksikkötestejä, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,24 @@ const totalLikes = (blogs) => {
   return sum
 }
 
+const favoriteBlog = (blogs) => {
+  let max = 0
+  let fav = 0
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes > max) {
+      max = blogs[i].likes
+      fav = i
+    }
+  }
+  const blog = blogs[fav]
+  delete blog._id
+  delete blog.url
+  delete blog.__v
+  return blog
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
